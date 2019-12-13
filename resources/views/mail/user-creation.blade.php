be4effcb0504d7f8499372c73e50e1c8d9b468e0
devy.viray <demetrio.viray@lafilgroup.com>
2019-12-13 15:48:51 +0800

    Change email content

--- resources/views/mail/user-creation.blade.php
+++ resources/views/mail/user-creation.blade.php
@@ -1,12 +1,11 @@
 <div>
-    <p>
-        Greetings!<br><br>
+    <p>Greetings!<br><br>
+        You have been registered to the 5S Portal. Click the link provided below and use the default password for access.<br><br>
 
-        You have been registered to the 5s Portal as {{ $role }}. To be able to access the portal you can go to <a href="{{ url('main') }}">5s-Portal</a> and use <strong> {{ $default_password }}</strong> as your password. Please be reminded to change you password on first log in. <br><br>
-        
-        Thank you very much!<br><br>
-        
-        Regards,<br>
-        CQA-QM 
+        Link: <a href="{{ url('main') }}">5s-Portal</a><br>
+        Default Password:<strong> {{ $default_password }}</strong><br><br>
+
+        Reminder: change your password upon log in.<br>
+        Thank you very much!<br>
     </p>
 </div>
