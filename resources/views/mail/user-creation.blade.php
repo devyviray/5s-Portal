e42b55cc26c0ff5d87870b808b07f997bc375e2f
devy.viray <demetrio.viray@lafilgroup.com>
2019-12-20 08:43:31 +0800

    Fixed url in creation of report

--- resources/views/mail/user-creation.blade.php
+++ resources/views/mail/user-creation.blade.php
@@ -2,7 +2,7 @@
     <p>Greetings!<br><br>
         You have been registered to the 5S Portal. Click the link provided below and use the default password for access.<br><br>
 
-        Link: <a href="{{ url('main') }}">5s-Portal</a><br>
+        Link: <a href="{{ url('login') }}">5s-Portal</a><br>
         Default Password:<strong> {{ $default_password }}</strong><br><br>
 
         Reminder: change your password upon log in.<br>
