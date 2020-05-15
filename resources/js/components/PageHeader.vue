39dc97482e06f471887dad1b114698c42458f9aa
devy.viray <demetrio.viray@lafilgroup.com>
2020-05-15 16:41:44 +0800

    height for logos

--- resources/js/components/PageHeader.vue
+++ resources/js/components/PageHeader.vue
@@ -3,11 +3,11 @@
         <nav class="navbar navbar-default top-navbar" role="navigation">
             <div class="row">
                 <div class="col-md-4">
-                    <img class="mt-4 ml-4 mb-4" :src="publicPath +'/img/new_design/5sPortal.png'">
+                    <img class="mt-3 ml-3 mb-3" :src="publicPath +'/img/new_design/5sPortal.png'">
                 </div>
                 <div class="col-md-4"></div>
                 <div class="col-md-4 text-right">
-                    <img class="mt-4 mr-4 mb-4" :src="publicPath +'/img/new_design/lfuggoc.png'">
+                    <img class="mt-3 mr-3 mb-3" :src="publicPath +'/img/new_design/lfuggoc.png'">
                 </div>
             </div>
         </nav>
