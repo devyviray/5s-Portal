f7b322cdfaac36e159009f93dc48c9b9c50c061f
LFUGGOC\stuartjames.yap <stuartjames.yap@lafilgroup.com>
2025-03-28 08:21:17 +0800

    export file cell adjustments

--- resources/views/user/export.blade.php
+++ resources/views/user/export.blade.php
@@ -1,27 +1,27 @@
 <table>
     <thead>
     <tr>
-		<th>ID</th>
-		<th colspan="2" style="font-weight: bold">Name</th>
-		<th colspan="2" style="font-weight: bold">Email</th>
-		<th colspan="4" style="font-weight: bold">Companies</th>
-		<th colspan="4" style="font-weight: bold">Department</th>
-		<th>Role</th>
+		<th style="font-weight: bold">ID</th>
+		<th style="font-weight: bold">NAME</th>
+		<th style="font-weight: bold">EMAIL</th>
+		<th style="font-weight: bold">COMPANIES</th>
+		<th style="font-weight: bold">DEPARTMENT</th>
+		<th style="font-weight: bold">ROLE</th>
     </tr>
     </thead>
     <tbody>
     @foreach($users as $user)
         <tr>
-		    <td>{{ $user->id }}</td>
-		    <td colspan="2">{{ $user->name }}</td>
-		    <td colspan="2">{{ $user->email }}</td>
-		    <td colspan="4">
+		    <td style="width: 50px">{{ $user->id }}</td>
+		    <td style="width: 200px">{{ $user->name }}</td>
+		    <td style="width: 300px">{{ $user->email }}</td>
+		    <td style="width: 500px">
                 @foreach($user->companies as $company)
 		    	<span>{{ $company->name }} - {{ $user->location->name }}<br></span>
                 @endforeach
 		    </td>
-		    <td colspan="4">{{ $user->department->name }}</td>
-		    <td>
+		    <td style="width: 450px">{{ $user->department->name }}</td>
+		    <td style="width: 200px">
                 @foreach($user->roles as $role)
 		    	<span>{{ $role->name }}<br></span>
                 @endforeach
